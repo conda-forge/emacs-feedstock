4e5ff9305dfaaa01e935e46f171ca3a389dbae77
Aaron Meurer <asmeurer@gmail.com>
2016-09-23 19:47:22 -0400

    Try just making symlinks and not doing what Homebrew does

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,5 @@
 if [ "$(uname)" == "Darwin" ]; then
-    OPTS="--disable-ns-self-contained --enable-locallisppath=$PREFIX/share/emacs/site-lisp"
+    OPTS=""
 else
     OPTS="--x-includes=$PREFIX/include --x-libraries=$PREFIX/lib"
 fi
@@ -11,14 +11,9 @@ make check
 make install
 
 if [ "$(uname)" == "Darwin" ]; then
-    # Based on
-    # https://github.com/Homebrew/homebrew-core/blob/master/Formula/emacs.rb
     mv nextstep/Emacs.app $PREFIX/Emacs.app
     mkdir -p $PREFIX/bin
-    rm $PREFIX/bin/emacs
-    cat <<EOF > $PREFIX/bin/emacs
-#!/bin/bash
-exec $PREFIX/Emacs.app/Contents/MacOS/Emacs "$@"
-EOF
-    chmod a+x $PREFIX/bin/emacs
+    ln -s $PREFIX/Emacs.app/Contents/MacOS/Emacs $PREFIX/bin/emacs-25.1
+    ln -s $PREFIX/bin/emacs-25.1 $PREFIX/bin/emacs
+    ln -s $PREFIX/Emacs.app/Contents/MacOS/Emacs/bin/* $PREFIX/bin/
 fi
