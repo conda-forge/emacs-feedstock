131ebb89c9702a34fb2044bba0e6a93735e70a92
Aaron Meurer <asmeurer@gmail.com>
2016-12-13 05:03:30 -0500

    Fix paths of symlinks on OS X build

--- recipe/build.sh
+++ recipe/build.sh
@@ -15,8 +15,8 @@ if [ "$(uname)" == "Darwin" ]; then
     mkdir -p $PREFIX/bin
     ln -s $PREFIX/Emacs.app/Contents/MacOS/Emacs $PREFIX/bin/emacs-25.1
     ln -s $PREFIX/bin/emacs-25.1 $PREFIX/bin/emacs
-    ln -s $PREFIX/Emacs.app/Contents/MacOS/Emacs/bin/ctags $PREFIX/bin/ctags
-    ln -s $PREFIX/Emacs.app/Contents/MacOS/Emacs/bin/ebrowse $PREFIX/bin/ebrowse
-    ln -s $PREFIX/Emacs.app/Contents/MacOS/Emacs/bin/emacsclient $PREFIX/bin/emacsclient
-    ln -s $PREFIX/Emacs.app/Contents/MacOS/Emacs/bin/etags $PREFIX/bin/etags
+    ln -s $PREFIX/Emacs.app/Contents/MacOS/bin/ctags $PREFIX/bin/ctags
+    ln -s $PREFIX/Emacs.app/Contents/MacOS/bin/ebrowse $PREFIX/bin/ebrowse
+    ln -s $PREFIX/Emacs.app/Contents/MacOS/bin/emacsclient $PREFIX/bin/emacsclient
+    ln -s $PREFIX/Emacs.app/Contents/MacOS/bin/etags $PREFIX/bin/etags
 fi
