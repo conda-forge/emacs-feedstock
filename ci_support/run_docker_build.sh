e7a2cc81364bea89d2731ead3ca8f958c499d39b
Aaron Meurer <asmeurer@gmail.com>
2017-05-17 14:34:26 -0400

    Revert "Update emacs to 25.2"

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -14,7 +14,7 @@ config=$(cat <<CONDARC
 
 channels:
  - conda-forge
- - defaults
+ - defaults # As we need conda-build
 
 conda-build:
  root-dir: /feedstock_root/build_artefacts
@@ -24,30 +24,14 @@ show_channel_urls: true
 CONDARC
 )
 
-# In order for the conda-build process in the container to write to the mounted
-# volumes, we need to run with the same id as the host machine, which is
-# normally the owner of the mounted volumes, or at least has write permission
-HOST_USER_ID=$(id -u)
-# Check if docker-machine is being used (normally on OSX) and get the uid from
-# the VM
-if hash docker-machine 2> /dev/null && docker-machine active > /dev/null; then
-    HOST_USER_ID=$(docker-machine ssh $(docker-machine active) id -u)
-fi
-
-rm -f "$FEEDSTOCK_ROOT/build_artefacts/conda-forge-build-done"
-
 cat << EOF | docker run -i \
-                        -v "${RECIPE_ROOT}":/recipe_root \
-                        -v "${FEEDSTOCK_ROOT}":/feedstock_root \
-                        -e HOST_USER_ID="${HOST_USER_ID}" \
+                        -v ${RECIPE_ROOT}:/recipe_root \
+                        -v ${FEEDSTOCK_ROOT}:/feedstock_root \
                         -a stdin -a stdout -a stderr \
                         condaforge/linux-anvil \
-                        bash || exit 1
+                        bash || exit $?
 
-set -e
-set +x
 export BINSTAR_TOKEN=${BINSTAR_TOKEN}
-set -x
 export PYTHONUNBUFFERED=1
 
 echo "$config" > ~/.condarc
@@ -62,17 +46,17 @@ source run_conda_forge_build_setup
 # "recipe/yum_requirements.txt" file. After updating that file,
 # run "conda smithy rerender" and this line be updated
 # automatically.
-/usr/bin/sudo -n yum install -y libXaw-devel
+yum install -y libXaw-devel
+
+# install conda-build 2.x to build a long prefix
+conda install --yes --quiet conda-build=2
+conda info
 
 
 # Embarking on 1 case(s).
     conda build /recipe_root --quiet || exit 1
     upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
-touch /feedstock_root/build_artefacts/conda-forge-build-done
-EOF
 
-# double-check that the build got to the end
-# see https://github.com/conda-forge/conda-smithy/pull/337
-# for a possible fix
-set -x
-test -f "$FEEDSTOCK_ROOT/build_artefacts/conda-forge-build-done" || exit 1
+# inspect the prefix lengths of the built packages
+conda inspect prefix-lengths /feedstock_root/build_artefacts/linux-64/*.tar.bz2
+EOF
