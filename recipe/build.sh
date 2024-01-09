e66dbe2875f21b347fe081c184a0948e9f602ac6
Aaron Meurer <asmeurer@gmail.com>
2024-01-08 18:47:05 -0700

    Use --with-tree-sitter in configure

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,7 +11,7 @@ if [ "$(uname)" == "Darwin" ]; then
     # https://github.com/conda-forge/emacs-feedstock/pull/16#issuecomment-334241528
     export LDFLAGS="${LDFLAGS} -ltinfo"
 else
-    OPTS="--x-includes=$PREFIX/include --x-libraries=$PREFIX/lib --with-x-toolkit=gtk3 --with-harfbuzz -with-cairo"
+    OPTS="--x-includes=$PREFIX/include --x-libraries=$PREFIX/lib --with-x-toolkit=gtk3 --with-harfbuzz -with-cairo --with-tree-sitter"
 fi
 
 autoreconf -vfi
